44db2e06c1bb258e088a9903f8c957b2cda00d19
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-06-07 00:08:40 +0000

    set up structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 
 function App() {
-  return <div className="App"></div>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
